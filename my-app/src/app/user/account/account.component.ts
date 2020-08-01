c8aa7226db75c63b0bf0abc8ca10b4e2c72e7fe5
jaxonflex <68178977+jaxonflex@users.noreply.github.com>
2020-07-31 18:03:43 -0600

    Update account.component.ts

--- my-app/src/app/user/account/account.component.ts
+++ my-app/src/app/user/account/account.component.ts
@@ -13,20 +13,34 @@ export class AccountComponent implements OnInit{
     constructor(public userListService: UserListService, public route: ActivatedRoute){}
     userList:UserListService;
     private paramID: string;
+    
 
     ngOnInit() {
         this.route.paramMap.subscribe((paramMap: ParamMap)=>{
             if(paramMap.has('accountID')) {
                 this.paramID = paramMap.get('accountID');
+                console.log("the result is: " + result);
                 var result = this.userListService.getSpecificAccount(this.paramID);
-                console.log("the sreulst is: " + result);
+                this.returnInfo(this.getAccountInfo,this.assignInfo);
+                console.log(result.accountID);
 
             }
 
         });
     }
 
-    getAccountIDFromURL(){
-
+    getAccountInfo(){
+        console.log("get account called");
+        return this.userListService.getSpecificAccount(this.paramID)
     }
+    assignInfo(results){
+        console.log(results);
+    }
+    returnInfo(getInfo,assignInfo){
+        console.log("reutrn info called");
+        var results = getInfo();
+        console.log("get info is done: " + results);
+        assignInfo(results);
+    }
+
 }
